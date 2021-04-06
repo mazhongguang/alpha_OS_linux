14a989f735d9c4bcba2673b436fa2d2447c8532f
mazhongguang <mazhongguang@163.com>
2021-04-06 11:48:35 +0800

    修改UBOOT里面默认的commond命令，直接查找mx6ul_mzg_emmc.dtb文件

--- uboot/uboot_mine/include/configs/mx6ull_mzg_emmc.h
+++ uboot/uboot_mine/include/configs/mx6ull_mzg_emmc.h
@@ -196,7 +196,7 @@
 				"if test $board_name = EVK && test $board_rev = 9X9; then " \
 					"setenv fdt_file imx6ull-9x9-evk.dtb; fi; " \
 				"if test $board_name = EVK && test $board_rev = 14X14; then " \
-					"setenv fdt_file imx6ull-14x14-evk.dtb; fi; " \
+					"setenv fdt_file imx6ull-mzg-emmc.dtb; fi; " \
 				"if test $fdt_file = undefined; then " \
 					"echo WARNING: Could not determine dtb to use; fi; " \
 			"fi;\0" \
