0ec06e8ed154cd6fdcdd18f995516fcdd340a41f
mazhongguang <mazhongguang@163.com>
2021-04-27 17:51:01 +0800

    06_gpioled PINCTRL子系统和GPIO子系统实验完成

---
+++ linux_drivers/06_gpioled/.config/ycm_flag.py
@@ -0,0 +1,15 @@
+#以下是YCM配置文件需要的FLAG内容
+flag = [ 
+'-I',
+'linux',
+'-I',
+'/home/user/linux/IMX6ULL/linux/linux_nxp_mine/include',
+'-I',
+'/home/user/linux/IMX6ULL/linux/linux_nxp_mine/arch/arm/include',
+'-I',
+'/home/user/linux/IMX6ULL/linux/linux_nxp_mine/arch/arm/include/generated',
+'-I',
+'/usr/include/x86_64-linux-gnu/asm',
+'-I',
+'/usr/include/x86_64-linux-gnu/sys',
+]
